c0050507eebaf8dbd19a1787697d857991cd7ec6
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-07-23 10:08:51 +0200

    préparation du merge

--- informationsPage.js
+++ informationsPage.js
@@ -1,1 +1,18 @@
- 
+export function informationsPage() {
+
+    const informationsPage = document.querySelector("#informationsPage");
+
+    informationsPage.innerHTML += `<h1 id="InformationsPageTitle">${APIResult.results[0].nom}</h1>`;
+
+    informationsPage.innerHTML += `<h2 InformationsPageAddress>${APIResult.results[0].adresse}, ${APIResult.results[0].commune}</h2>`;
+
+    informationsPage.innerHTML += `<ul><li>${APIResult.results[0].fauteuils} fauteuils</li><li>${APIResult.results[0].ecrans} écran(s)</li></ul>`;
+
+    informationsPage.innerHTML += `<iframe src="https://data.culture.gouv.fr/explore/embed/dataset/etablissements-cinematographiques/map/?location=15,${APIResult.results[0].latitude},${APIResult.results[0].longitude}&static=true&datasetcard=false&scrollWheelZoom=false" width="600" height="600" frameborder="0"></iframe>`;
+
+
+    informationsPage.innerHTML += ``;
+    informationsPage.innerHTML += ``;
+    informationsPage.innerHTML += ``;
+
+};
--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 // import { searchPage } from "./searchPage.js";
 // import { resultPage } from "./resultPage.js";
-// import { informationsPage } from "./informationsPage.js";
+import { informationsPage } from "./informationsPage.js";
 import { APIResult } from "./APIResult.js";
 console.log(APIResult);
 
-const informationsPage = document.querySelector("#informationsPage");
-const statList = document.querySelector("#statList");
-
-
-informationsPage.innerHTML += `<h1 id="InformationsPageTitle">${APIResult.results[7].nom}</h1>`;
-
-informationsPage.innerHTML += `<h2 InformationsPageAddress>${APIResult.results[7].adresse}, ${APIResult.results[7].commune}</h2>`;
-
-statList.innerHTML += `<li>${APIResult.results[7].fauteuils} fauteuils</li>`;
-statList.innerHTML += `<li>${APIResult.results[7].ecrans} écran(s)</li>`;
-
-// informationsPage.innerHTML += `<iframe src="https://data.culture.gouv.fr/explore/embed/dataset/etablissements-cinematographiques/map/?location=15,${APIResult.results[7].latitude},${APIResult.results[7].longitude}&static=true&datasetcard=false&scrollWheelZoom=false" width="600" height="600" frameborder="0"></iframe>`;
-
-
-informationsPage.innerHTML += ``;
-informationsPage.innerHTML += ``;
-informationsPage.innerHTML += ``;
-
+console.log(informationsPage());
 
 
 // async function fetchGeocodage(adress) {
@@ -31,17 +14,3 @@ informationsPage.innerHTML += ``;
 
 //     console.log(data.features[0].geometry.coordinates);
 // };
-
-// on attend Vi pour cette section
-// const expandArea = (latitude, longitude ) => {
-//     const chosenDistance = 0.0134;
-
-//     const latitudeMoins = latitude - chosenDistance;
-//     const latitudePlus = latitude + chosenDistance;
-//     const longMoins = longitude - chosenDistance;
-//     const longPlus = longitude + chosenDistance;
-
-//     return [longPlus, longMoins, latitudePlus, latitudeMoins];
-// };
-
-// console.log(expandArea(48.87393997249049, 2.358557443188446));
